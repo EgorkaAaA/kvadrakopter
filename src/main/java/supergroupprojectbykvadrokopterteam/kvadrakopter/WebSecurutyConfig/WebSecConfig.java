e64343f243ed64e9b7228f7041ff5350c89e7f7c
Егор Лебедев <m123ry132@gmail.com>
2021-09-11 14:38:09 +0700

    Create password bcrypt password encoder and start configure dao authentication provider

--- src/main/java/supergroupprojectbykvadrokopterteam/kvadrakopter/WebSecurutyConfig/WebSecConfig.java
+++ src/main/java/supergroupprojectbykvadrokopterteam/kvadrakopter/WebSecurutyConfig/WebSecConfig.java
@@ -1,13 +1,29 @@
 package supergroupprojectbykvadrokopterteam.kvadrakopter.WebSecurutyConfig;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class WebSecConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.httpBasic();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService();
     }
 }
